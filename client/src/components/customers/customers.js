81d17b17dc41c4bcf23f912cf90cf93d8180bd6d
jwaltrip <jake.waltrip@gmail.com>
2018-10-09 16:27:24 -0500

    initial commit, simple React/Express boilerplate

---
+++ client/src/components/customers/customers.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './customers.css';
+
+class Customers extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      customers: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api/customers')
+      .then(res => res.json())
+      .then(customers => {
+        this.setState({ customers: customers });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Customers</h2>
+        <ul>
+          {this.state.customers.map((customer, idx) => {
+            return <li key={idx}> { customer.firstName } { customer.lastName } </li>
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Customers;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/api/customers', (req, res) => {
+  // hardcoded data to start with
+  // this data will usually come from mongoDB
+  const customers = [
+    {id: 1, firstName: 'Jake', lastName: 'Waltrip'},
+    {id: 2, firstName: 'Laurie', lastName: 'Hansson'},
+    {id: 3, firstName: 'John', lastName: 'Doe'}
+  ];
+
+  res.json(customers);
+});
+
+const port = 5000;
+
+app.listen(port, ()=> console.log(`Server started on port ${port}`));
